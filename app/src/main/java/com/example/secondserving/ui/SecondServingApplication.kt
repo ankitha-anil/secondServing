c13e7682cea6df2264ec76e420e776c58522e35d
Jerome Chin <jerome.chin@ncs.com.sg>
2024-02-23 22:38:23 +0800

    initialize and build db in main app file

--- app/src/main/java/com/example/secondserving/ui/SecondServingApplication.kt
+++ app/src/main/java/com/example/secondserving/ui/SecondServingApplication.kt
@@ -1,9 +1,24 @@
 package com.example.secondserving.ui
 
 import android.app.Application
+import androidx.room.Room
+import com.example.secondserving.data.db.AppDatabase
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
 class SecondServingApplication: Application() {
+    // build db
+    lateinit var db: AppDatabase
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "ssdb"
+        ).build()
+    }
+
+
 }
