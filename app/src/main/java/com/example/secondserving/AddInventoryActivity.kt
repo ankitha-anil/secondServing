7acc868b5c6a6e5f4214e404f85c0d0a47996fd0
NTUniverse <ntuniverseproject@gmail.com>
2024-02-25 01:00:39 +0800

    Created "Add" button for Inventory page,Creating Add Inventory activity

---
+++ app/src/main/java/com/example/secondserving/AddInventoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.secondserving
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddInventoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_inventory)
+    }
+}
