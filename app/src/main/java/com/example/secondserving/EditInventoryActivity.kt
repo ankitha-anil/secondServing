b42a0c91d5ff3a993c3859c84855e5ca66cef860
TYJ-GitHub <teoyijie.main@gmail.com>
2024-03-04 23:35:21 +0800

    View and Edit UI

---
+++ app/src/main/java/com/example/secondserving/EditInventoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.secondserving
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditInventoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_inventory)
+    }
+}
