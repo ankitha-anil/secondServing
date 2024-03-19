e0d59e71a0a392319c35943947b01161660734b8
Jerome Chin <jerome.chin@ncs.com.sg>
2024-03-19 21:11:44 +0800

    add builder

--- app/src/main/java/com/example/secondserving/ui/SecondServingApplication.kt
+++ app/src/main/java/com/example/secondserving/ui/SecondServingApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.room.Room
 import dagger.hilt.android.HiltAndroidApp
 import com.example.secondserving.data.InventoryDatabase
+import com.example.secondserving.data.RecipeDatabase
 
 @HiltAndroidApp
 class SecondServingApplication: Application() {
@@ -11,6 +12,13 @@ class SecondServingApplication: Application() {
         Room.databaseBuilder(
             applicationContext,
             InventoryDatabase::class.java, "inventory_database"
+        ).build()
+    }
+
+    val data1: RecipeDatabase by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            RecipeDatabase::class.java, "recipe_database"
         ).build()
     }
 }
